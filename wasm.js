341979c8368c36b486c178716d4875d9b234187d
Ian Douglas Scott <ian@iandouglasscott.com>
2019-08-07 09:39:39 -0700

    Configure rewind structure in call()

--- wasm.js
+++ wasm.js
@@ -15,35 +15,35 @@ const STACK_SIZE = 1024;
 class ForkHandler {
     call(mem, instance, data_addr, stack_size, f, create_fork) {
         this.instance = instance;
-        this.view = new Int32Array(instance.exports.memory.buffer);
         this.data_addr = data_addr;
-        this.stack_size = stack_size;
         this.child_pid = 0;
         this.forking = (mem !== undefined);
 
+        let view = new Int32Array(instance.exports.memory.buffer);
+
         if (mem) {
-            this.view.set(mem);
+            view.set(mem);
         }
+
+        view[data_addr / 4] = data_addr + 8;
+        view[data_addr / 4 + 1] = data_addr + 8 + stack_size;
 
         do {
             if (this.forking) {
-                this.instance.exports.asyncify_start_rewind(data_addr);
+                instance.exports.asyncify_start_rewind(data_addr);
             }
 
             f();
 
             if (this.forking) {
-                this.instance.exports.asyncify_stop_unwind();
-                this.child_pid = create_fork(this.view);
+                instance.exports.asyncify_stop_unwind();
+                this.child_pid = create_fork(view);
             }
         } while(this.forking);
     }
 
     fork() {
         if (!this.forking) {
-            this.view[this.data_addr / 4] = this.data_addr + 8;
-            this.view[this.data_addr / 4 + 1] = this.data_addr + 8 + this.stack_size;
-
             this.instance.exports.asyncify_start_unwind(this.data_addr);
         } else {
             this.instance.exports.asyncify_stop_rewind();
