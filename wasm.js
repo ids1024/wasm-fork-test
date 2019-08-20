71756328d9d312d7c6f50095687fd1af653eb63e
Ian Douglas Scott <ian@iandouglasscott.com>
2019-08-20 09:41:05 -0700

    Seperate instance loading and calling

--- wasm.js
+++ wasm.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 'use strict';
 
 // https://kripken.github.io/blog/wasm/2019/07/16/asyncify.html
@@ -13,33 +15,35 @@ const STACK_SIZE = 1024;
 
 
 class ForkHandler {
-    call(mem, instance, data_addr, stack_size, f, create_fork) {
+    load_instance(mem, instance, data_addr, stack_size, create_fork) {
         this.instance = instance;
         this.data_addr = data_addr;
         this.child_pid = 0;
         this.forking = (mem !== undefined);
-
-        let view = new Int32Array(instance.exports.memory.buffer);
+        this.create_fork = create_fork;
+        this.view = new Int32Array(instance.exports.memory.buffer);
 
         // If we are a new process created in a fork, load memory
         if (mem) {
-            view.set(mem);
+            this.view.set(mem);
         }
 
         // Configure the structure asyncify uses to save stack
-        view[data_addr / 4] = data_addr + 8;
-        view[data_addr / 4 + 1] = data_addr + 8 + stack_size;
+        this.view[data_addr / 4] = data_addr + 8;
+        this.view[data_addr / 4 + 1] = data_addr + 8 + stack_size;
+    }
 
+    call(function_name, ...args) {
         do {
             if (this.forking) {
-                instance.exports.asyncify_start_rewind(data_addr);
+                this.instance.exports.asyncify_start_rewind(this.data_addr);
             }
 
-            f();
+            this.instance.exports[function_name](...args);
 
             if (this.forking) {
-                instance.exports.asyncify_stop_unwind();
-                this.child_pid = create_fork(view);
+                this.instance.exports.asyncify_stop_unwind();
+                this.child_pid = this.create_fork(this.view);
             }
         } while(this.forking);
     }
@@ -69,11 +73,13 @@ async function main(mem) {
     let instance = (await WebAssembly.instantiate(wasm, {env: env})).instance;
     let data_addr = instance.exports.__heap_base.value;
 
-    fork_handler.call(mem, instance, data_addr, STACK_SIZE, instance.exports.main, view => {
+    fork_handler.load_instance(mem, instance, data_addr, STACK_SIZE, view => {
         let child = child_process.fork(JS_PATH, ['--forked']);
         child.send(Array.from(view));
         return child.pid;
     });
+
+    fork_handler.call("main");
 }
 
 
